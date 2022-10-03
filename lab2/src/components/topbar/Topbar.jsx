294c05fddc46c963b2fbc2f8b5bc83e8fbb5dfa3
altangerel2001 <112841569+altangerel2001@users.noreply.github.com>
2022-10-03 19:37:19 +0800

    nice

--- lab2/src/components/topbar/Topbar.jsx
+++ lab2/src/components/topbar/Topbar.jsx
@@ -1,11 +1,34 @@
 import "./topbar.css"
-import { Search } from "@mui/icons-material"
+import { Search, Person,Notifications,Chat} from "@mui/icons-material"
 export default function Topbar() {
   return (
   <div className="topbarContainer"> 
   <div className="topbarLeft"> <span className="logo">lamasocail</span></div>
-  <div className="topbarCenter"> <div className="searchbar"><Search/></div></div>
-  <div className="topbarRight"></div>
+  <div className="topbarCenter"> <div className="searchbar">
+    <Search className="searchicon" />
+    <input placeholder="Search for freind, post or video" classname="Searchinput"/>
+    </div></div>
+  <div className="topbarRight">
+    <div className="topbarLinks"> 
+    <span className="topbarLinks">Homepage</span>
+    <span className="topbarLinks">Timeline</span>
+    </div>
+    <div className="topbaricons">
+     <div className="topbariconsitem">
+      <Person/>
+      <span className="topbariconbadge">100</span>
+     </div>
+     <div className="topbariconsitem">
+      <Notifications/>
+      <span className="topbariconbadge">12</span>
+     </div>
+     <div className="topbariconsitem">
+      <Chat/>
+      <span className="topbariconbadge">6</span>
+     </div>
+    </div>
+    <img src="/assets/oprooa/1.jpeg" alt="" className="topbarimg" />
+  </div>
     </div>
   )
 }
