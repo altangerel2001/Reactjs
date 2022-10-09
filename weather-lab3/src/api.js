df7f8fe8e3d658a9ed933521ea8e457e0ab863d1
altangerel2001 <112841569+altangerel2001@users.noreply.github.com>
2022-10-09 18:24:16 +0800

    aaaa

---
+++ weather-lab3/src/api.js
@@ -0,0 +1,10 @@
+export const geoApioptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'ff76a69b60msh8e4cd29d0070c0ep1862e7jsncbb189abc8da',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+
--- weather-lab3/src/components/search/search.js
+++ weather-lab3/src/components/search/search.js
@@ -1,5 +1,45 @@
-const Search = () => {
-    return ('Hello')
+import { useState } from "react";
+import { AsyncPaginate } from "react-select-async-paginate";
+import { GEO_API_URL, geoApioptions } from "../../api";
+
+const Search = ({ onSearchChange }) => {
+
+    const [search, setSearch,] = useState(null);
+
+    const loadOptions = (inputValue) => {
+        return fetch(`${GEO_API_URL}/cities?minPopulation=1000000$namePrefix=${inputValue}`,geoApioptions)
+            .then(response => response.json())
+            .then(response => {
+                return {
+                    options: response.data.map((city) => {
+                        return{
+                            value: , 
+                            label: , 
+                        }
+                    }
+                    )
+                }
+            }
+            )
+            .catch(err => console.error(err));
+    }
+
+
+
+    const handleOnChange = (searchData) => {
+        setSearch(searchData);
+        onSearchChange(searchData);
+    }
+
+    return (
+        <AsyncPaginate
+            placeholder="Search for city"
+            debounceTimeout={600}
+            value={search}
+            onChange={handleOnChange}
+            loadOptions={loadOptions}
+        />
+    )
 }
 
 export default Search;
