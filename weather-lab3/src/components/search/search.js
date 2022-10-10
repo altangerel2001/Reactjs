5c15ff7ac4adadaedfb8e9dcb24b01161c7ffbc4
sw21d039 <monster_altan@yahoo.com>
2022-10-10 12:51:48 +0800

    asasdsda

--- weather-lab3/src/components/search/search.js
+++ weather-lab3/src/components/search/search.js
@@ -13,14 +13,15 @@ const Search = ({ onSearchChange }) => {
                 return {
                     options: response.data.map((city) => {
                         return{
-                            value: , 
-                            label: , 
+                            value: `${city.latitude} ${city.longitude}`,
+                     
+                            label: `${city.name}, ${city.countryCode}` , 
                         }
                     }
                     )
                 }
             }
-            )
+         )
             .catch(err => console.error(err));
     }
 
