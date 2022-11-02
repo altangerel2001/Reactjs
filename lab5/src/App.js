78892166aa3c2ec1e191a99b171d7211acb4da8b
altangerel2001 <112841569+altangerel2001@users.noreply.github.com>
2022-11-02 21:14:06 +0800

    aaaa

--- lab5/src/App.js
+++ lab5/src/App.js
@@ -1,9 +1,16 @@
+import Catagory from './components/Catagory';
 import Pages from './pages/Pages'
+import {BrowserRouter} from "react-router-dom" 
+import Search from './components/Search';
+
 function App() {
   return (
     <div className="App">
-      <Pages />
-      
+      <BrowserRouter>
+        <Search/>
+        <Catagory />
+        <Pages />
+      </BrowserRouter>
     </div>
   );
 }
---
+++ lab5/src/components/Catagory.jsx
@@ -0,0 +1,75 @@
+import {FaHamburger, FaPizzaSlice,} from "react-icons/fa";
+import {GiNoodles,  GiChopsticks} from "react-icons/gi";
+import { NavLink} from "react-router-dom";
+import styled from "styled-components";
+
+
+function Catagory() {
+
+    return (
+        <List>
+            <SLink to={'/cuisine/Italian'}>
+                <FaPizzaSlice/>
+                <h4>Italian</h4>
+            </SLink>
+            <SLink to={'/cuisine/American'}>
+                <FaHamburger/>
+                <h4>Italian</h4>
+            </SLink>
+            <SLink to={'/cuisine/Thai'}>
+                <GiChopsticks/>
+                <h4>Japanense</h4>
+            </SLink>
+            <SLink to={'/cuisine/Japanese'}>
+                <GiNoodles/>
+                <h4>Thai</h4>
+            </SLink>
+        </List>
+    )
+    
+
+
+
+}
+const List = styled.div`
+    display: flex;
+    justify-content: center;
+    margin: 2rem 0rem;
+
+
+`;
+const SLink = styled(NavLink)`
+display: flex;
+flex-diretion: column;
+justify-content: center;
+align-items: center;
+border-radius: 50%;
+margin-right: 2rem;
+text-decoration: none;
+background: linear-gradient(35deg, #494949, #313131 );
+width:6rem;
+height: 6rem;
+cursor: pointer;
+transform: scale(0.8rem);
+
+h4 {
+    color: white;
+    font-size: 0.8rem;
+}
+svg{
+    color: white;
+    font-size: 1.5rem;
+}
+&.active{
+    background: linear-gradient(to right, #f27121, #e94057);
+    svg{
+        color: white;
+    }
+    h4{
+        color: white;
+    }
+}
+
+`
+
+export default Catagory
