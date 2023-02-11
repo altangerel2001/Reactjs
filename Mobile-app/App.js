f8e8020e1a5ece866ef5cfca7c4a8fc03da28e7e
altangerel2001 <112841569+altangerel2001@users.noreply.github.com>
2023-02-11 20:52:32 +0800

    mobile app
    lab

---
+++ Mobile-app/App.js
@@ -0,0 +1,69 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image, Pressable, ScrollView, SafeAreaView, Button, TextInput } from 'react-native';
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+    <ScrollView>
+    <View style={{marginTop: 55}}>
+
+      <Pressable onPress={()=>alert('та нийтлэл бичих үүу?')} onLongPress={()=>alert('та өөрт таалагдсан нийтлэлээ бичихдээ нийтлэл хэсэгт нэг удаа дарна уу')}>
+      <Text style={{textAlignVertical: "center",textAlign: "center",}} >Та өөрт таалагдсан зүйлээ нийтлээрэй</Text>
+      </Pressable>
+
+      <TextInput 
+        style={styles.input}
+        />
+
+      <Button
+      onPress={()=>alert('та нийтлэл бичих үү')}
+      title="нийтлэл"
+      color="black"
+      style={{width:30}}
+      />
+      
+      <Pressable 
+      onPress={()=>alert("та зурган дээрээ удаан дарж зургаа солино уу?")} 
+      onLongPress={()=>alert("та зургаа солих уу?")}>
+      <Image source={require("./assets/as.jpg")} style={{height:250, width:'100%'}}></Image>
+      </Pressable>
+      
+      <Button
+      onPress={()=>alert('та товчийг дарсан байна!')}
+      title="BUTTON"
+      color="black"
+      style={{width:300}}
+
+      />
+    
+
+      <Image source={require("./assets/e84ac7e579f3bca5def4193d2c00398a.jpg")} style={{height:1000, width:400}}></Image>
+      
+    
+
+      <StatusBar style="auto" />
+    </View>
+     </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor:"#fff",
+    alignItems:'center',
+    justifyContent:'center',
+
+  },
+  text: {
+    fontSize: 42,
+  },
+  input:{
+    height:40,
+    margin:25,
+    borderWidth:1,
+    padding:10,
+    borderColor:'black',
+  },
+});
--- socialapp/src/components/closeFreind/CloseFreind.jsx
+++ socialapp/src/components/closeFreind/CloseFreind.jsx
@@ -1,10 +1,12 @@
-import "./closeFriend.css";
+/** @format */
 
-export default function CloseFriend({user}) {
+// import "./closeFriend.css";
+
+export default function CloseFriend({ user }) {
   return (
-    <li className="sidebarFriend">
-      <img className="sidebarFriendImg" src={user.profilePicture} alt="" />
-      <span className="sidebarFriendName">{user.username}</span>
+    <li className='sidebarFriend'>
+      <img className='sidebarFriendImg' src={user.profilePicture} alt='' />
+      <span className='sidebarFriendName'>{user.username}</span>
     </li>
   );
 }
